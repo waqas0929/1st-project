287bae0b896beeed113f2df67f1e7901b66c1af5
waqas0929 <waqasraza0929@gmail.com>
2024-05-14 11:19:15 +0500

    add .ignore and .env file in project

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
-import express from "express";
+import 'dotenv/config'
 
+import express from "express";
 import allRoutes from "./routes/index.js";
+import { connectDB } from "./db/config.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(allRoutes);
+
+connectDB();
 
 app.listen(3002, () => {
   console.log("server started");
---
+++ src/db/config.js
@@ -0,0 +1,24 @@
+import { Sequelize } from "sequelize";
+
+const dbName = process.env.DB_NAME;
+const dbUsername = process.env.DB_USERNAME;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+console.log(dbName, dbUsername, dbPassword)
+const sequelize = new Sequelize(dbName, dbUsername, dbPassword, {
+  host: dbHost,
+  dialect: "postgres",
+});
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+export { connectDB };
+
+export default Sequelize;
